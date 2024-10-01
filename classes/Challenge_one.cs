12bea76205f6fcbee23b35746fb779cc2d17b00a
SAM082024 <sam082024@gmail.com>
2024-10-01 13:22:30 +0200

    added a for-loop function to print numbers from start to end

--- classes/Challenge_one.cs
+++ classes/Challenge_one.cs
@@ -3,6 +3,10 @@ public class Challenge_one
     public void PrintNumbers(int start, int end)
     {
         // Todo: Print out the numbers 1 - 10
+        for (int i = start; i <= end; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 
     public void PrintStrings(string[] arr)
